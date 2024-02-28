d550d07465fbcbb811eff3cfd72778330053fb50
madorPele1 <159686722+madorPele1@users.noreply.github.com>
2024-02-28 10:05:27 +0200

    sort by letter in category-search.js

--- category-search.js
+++ category-search.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', initCategorySearch)
+document.addEventListener('DOMContentLoaded', initCategorySearch);
 
 function topFunction() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
-  })
+  });
 }
 
 function initCategorySearch() {
-  let dictionaryData = JSON.parse(localStorage.getItem('dictionaryData'))
+  let dictionaryData = JSON.parse(localStorage.getItem('dictionaryData'));
 
-  const categoryLinksContainer = document.querySelector('.container')
-  const searchResultsList = document.getElementById('searchResultsList')
-  const searchResultsCategory = document.getElementById('searchResultsCategory')
-  const searchQueryMessage = document.getElementById('searchQueryMessage')
+  const categoryLinksContainer = document.querySelector('.container');
+  const searchResultsList = document.getElementById('searchResultsList');
+  const searchResultsCategory = document.getElementById('searchResultsCategory');
+  const searchQueryMessage = document.getElementById('searchQueryMessage');
 
   function markSelectedCategory(categoryLink) {
-    const categoryLinks =
-      categoryLinksContainer.querySelectorAll('.category-link')
+    const categoryLinks = categoryLinksContainer.querySelectorAll('.category-link');
     categoryLinks.forEach((link) => {
-      link.removeAttribute('transition-style', 'in:wipe:right')
-      link.classList.remove('selected')
-    })
-    categoryLink.classList.add('selected')
-    categoryLink.setAttribute('transition-style', 'in:wipe:right')
+      // link.removeAttribute('transition-style', 'in:wipe:right');
+      link.classList.remove('selected');
+    });
+    categoryLink.classList.add('selected');
+    // categoryLink.setAttribute('transition-style', 'in:wipe:right');
   }
 
   function displayWords(words) {
@@ -31,38 +30,30 @@ function initCategorySearch() {
       .map(
         ({ word, translation, category: wordCategories, definition }) => `
         <li>
-          <p style="font-size: 30px; margin: 0;">${word} [${wordCategories.join(
-          ', '
-        )}]</p>
+          <p style="font-size: 30px; margin: 0;">${word} [${wordCategories.join(', ')}]</p>
           <p style="color: orange; font-size: 20px;">${translation}</p>
           <p style="font-size: 25px;">${definition}</p>
         </li>
       `
       )
-      .join('')
+      .join('');
   }
 
-  function filterWordsByCategory(category) {
+  function filterWordsByCategoryAndSort(category) {
     const filteredWords = dictionaryData.words.filter(
       ({ category: wordCategories }) => wordCategories.includes(category)
-    )
-
-    if (filteredWords.length > 0) {
-      searchResultsCategory.style.display = 'block'
-      // searchQueryMessage.textContent = `מילים בקטגוריה "${category}":`
-      displayWords(filteredWords)
-    } else {
-      searchResultsCategory.style.display = 'none'
-      searchQueryMessage.textContent = `אין מילים בקטגוריה "${category}".`
-    }
+    );
+      filteredWords.sort((a, b) => a.word.localeCompare(b.word));
+      searchResultsCategory.style.display = 'block';
+      displayWords(filteredWords);
   }
 
-  const categoryLinks =
-    categoryLinksContainer.querySelectorAll('.category-link')
+  const categoryLinks = categoryLinksContainer.querySelectorAll('.category-link');
   categoryLinks.forEach((categoryLink) => {
     categoryLink.addEventListener('click', () => {
-      filterWordsByCategory(categoryLink.textContent)
-      markSelectedCategory(categoryLink)
-    })
-  })
+      const category = categoryLink.textContent;
+      filterWordsByCategoryAndSort(category);
+      markSelectedCategory(categoryLink);
+    });
+  });
 }
